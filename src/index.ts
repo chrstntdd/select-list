b8fcdecbe94e4bdd0ca4ec4ebaf733bbd19d2eb2
Christian Todd <christiantodd27@icloud.com>
2018-08-26 16:06:12 -0400

    initial class rewrite with updated tests and updated project config.

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,96 @@
-function SelectList(arr: any[], selectedIndex?: number) {
-  selectedIndex = selectedIndex || 0;
-  let before: any[] = arr.slice(0, selectedIndex);
-  let selected: any = arr[selectedIndex];
-  let after: any[] = arr.slice(selectedIndex + 1);
+class SelectList<a> {
+  private _before: a[];
+  private _selected: a;
+  private _after: a[];
 
-  return {
-    /**
-     * @description
-     * Shift forwards by one element unless there are no more elements in the `after` section
-     */
-    next(): void {
-      if (after.length) {
-        const lastCurrent = selected;
-        const nextCurrent = after.shift();
-        before.push(lastCurrent);
-        selected = nextCurrent;
-      }
-    },
-    /**
-     * @description
-     * Shift back by one element unless there are no more elements in the `before` section
-     */
-    prev(): void {
-      if (before.length) {
-        const lastCurrent = selected;
-        const nextCurrent = before.shift();
-        after.unshift(lastCurrent);
-        selected = nextCurrent;
-      }
-    },
-    /**
-     * @description
-     * Return the entire collection as a single array
-     */
-    toArray(): any[] {
-      return before.concat([selected], after);
-    },
-    /**
-     * @description
-     * Return the size of the entire collection
-     */
-    get size(): number {
-      return before.length + 1 + after.length;
-    },
-    /**
-     * @description
-     * Return the currently selected element
-     */
-    get selected(): any {
-      return selected;
-    },
-    /**
-     * @description
-     * Return the elements currently in the `before` section of the SelectList
-     */
-    get before(): any[] {
-      return before;
-    },
-    /**
-     * @description
-     * Return the elements currently in the `after` section of the SelectList
-     */
-    get after(): any[] {
-      return after;
+  constructor(arr: a[], selectedIndex?: number) {
+    selectedIndex = selectedIndex || 0;
+    this._before = arr.slice(0, selectedIndex);
+    this._selected = arr[selectedIndex];
+    this._after = arr.slice(selectedIndex + 1);
+  }
+
+  /**
+   * @description
+   * Shift forwards by one element unless there are no more elements in the `after` section
+   */
+  public next(): SelectList<a> {
+    if (this._after.length) {
+      const afterCopy = [...this._after];
+      const nextCurrent = afterCopy.shift();
+
+      return new SelectList(
+        [...this._before, this._selected, nextCurrent, ...afterCopy],
+        this._before.length + 1
+      );
     }
-  };
+    return this;
+  }
+
+  /**
+   * @description
+   * Shift back by one element unless there are no more elements in the `before` section
+   */
+  public prev(): SelectList<a> {
+    if (this._before.length) {
+      const beforeCopy = [...this._before];
+      const nextCurrent = beforeCopy.pop();
+
+      return new SelectList(
+        [...beforeCopy, nextCurrent, this._selected, ...this._after],
+        this._before.length - 1
+      );
+    }
+    return this;
+  }
+
+  /**
+   * @description
+   * Apply a function to every element in the SelectList
+   */
+  public map<b>(fn: (element: a, index?: number, array?: a[]) => b): SelectList<b> {
+    return new SelectList(
+      [...this._before.map(fn), fn(this._selected), ...this._after.map(fn)],
+      this._before.length
+    );
+  }
+
+  /**
+   * @description
+   * Return the entire collection as a single array
+   */
+  public toArray(): a[] {
+    return [...this._before, this._selected, ...this._after];
+  }
+
+  /**
+   * @description
+   * Return the size of the entire collection
+   */
+  public size(): number {
+    return this._before.length + 1 + this._after.length;
+  }
+  /**
+   * @description
+   * Return the currently selected element
+   */
+  public selected(): a {
+    return this._selected;
+  }
+  /**
+   * @description
+   * Returns a shallow copy of the elements currently in the `before` section of the SelectList
+   */
+  public before(): a[] {
+    return [...this._before];
+  }
+  /**
+   * @description
+   * Returns a shallow copy of the elements currently in the `after` section of the SelectList
+   */
+  public after(): a[] {
+    return [...this._after];
+  }
 }
 
 export default SelectList;
--- test/SelectList.spec.ts
+++ test/SelectList.spec.ts
@@ -6,108 +6,131 @@ describe('SelectList', () => {
   describe('initialization', () => {
     describe('when a selectedIndex is provided', () => {
       it('should set the `selected` element to the provided index', () => {
-        const sel = SelectList(testArr);
+        const sel = new SelectList(testArr);
 
-        expect(sel.selected).toStrictEqual(testArr[0]);
+        expect(sel.selected()).toEqual(testArr[0]);
       });
     });
+
     describe('when a selectedIndex is **NOT** provided', () => {
       it('should set the `selected` element the first element in the input collection', () => {
-        const sel = SelectList(testArr, 3);
+        const sel = new SelectList(testArr, 3);
 
-        expect(sel.selected).toStrictEqual(testArr[3]);
+        expect(sel.selected()).toEqual(testArr[3]);
       });
     });
   });
-  describe('next function', () => {
+
+  describe('next method', () => {
     describe('when there are elements in the `after` collection', () => {
-      it('should advance the select list forwards by one element', () => {
-        const sel = SelectList(testArr);
+      it('should advance the select list forwards by one element and return a new SelectList', () => {
+        const sel = new SelectList(testArr);
 
-        expect(sel.selected).toStrictEqual(testArr[0]);
+        const next = sel.next();
 
-        sel.next();
-
-        expect(sel.selected).toStrictEqual(testArr[1]);
+        expect(next.selected()).toEqual(testArr[1]);
+        expect(next).not.toEqual(sel);
       });
     });
 
     describe('when there are **NOT** elements in the `after` collection', () => {
       it('should not alter the SelectList', () => {
-        const sel = SelectList(testArr, 4);
+        const sel = new SelectList(testArr, 4);
 
-        expect(sel.selected).toStrictEqual(testArr[4]);
+        expect(sel.selected()).toEqual(testArr[4]);
 
-        sel.next();
+        const next = sel.next();
 
-        expect(sel.selected).toStrictEqual(testArr[4]);
+        expect(next.selected()).toEqual(testArr[4]);
+        expect(next).toEqual(sel);
       });
     });
   });
 
-  describe('prev function', () => {
+  describe('prev method', () => {
     describe('when there are elements in the `before` collection', () => {
-      it('should advance the select list forwards by one element', () => {
-        const sel = SelectList(testArr, 1);
+      it('should shift the select list back by one element and return a new SelectList', () => {
+        const sel = new SelectList(testArr, 2);
 
-        expect(sel.selected).toStrictEqual(testArr[1]);
+        expect(sel.selected()).toEqual(testArr[2]);
 
-        sel.prev();
+        const prev = sel.prev();
 
-        expect(sel.selected).toStrictEqual(testArr[0]);
+        expect(prev.selected()).toEqual(testArr[1]);
+        expect(prev).not.toEqual(sel);
       });
     });
 
     describe('when there are **NOT** elements in the `before` collection', () => {
       it('should not alter the SelectList', () => {
-        const sel = SelectList(testArr);
+        const sel = new SelectList(testArr);
 
-        expect(sel.selected).toStrictEqual(testArr[0]);
+        expect(sel.selected()).toEqual(testArr[0]);
 
-        sel.prev();
+        const prev = sel.prev();
 
-        expect(sel.selected).toStrictEqual(testArr[0]);
+        expect(prev.selected()).toEqual(testArr[0]);
+        expect(prev).toEqual(sel);
       });
     });
   });
 
-  describe('toArray function', () => {
+  describe('map method', () => {
+    it('should apply a function to each element in the SelectList', () => {
+      const sel = new SelectList(testArr);
+
+      const transformation = sel.map(el => el.toUpperCase()).toArray();
+
+      transformation.forEach((el, i) => expect(el).toEqual(sel.toArray()[i].toUpperCase()));
+      expect(sel).not.toEqual(transformation);
+    });
+  });
+
+  describe('toArray method', () => {
     it('should return the complete collection', () => {
-      const sel = SelectList(testArr);
+      const sel = new SelectList(testArr);
 
-      expect(sel.toArray()).toStrictEqual(testArr);
+      expect(sel.toArray()).toEqual(testArr);
     });
   });
 
-  describe('size getter', () => {
+  describe('size method', () => {
     it('should return length of the complete collection', () => {
-      const sel = SelectList(testArr);
+      const sel = new SelectList(testArr);
 
-      expect(sel.size).toStrictEqual(testArr.length);
+      expect(sel.size()).toEqual(testArr.length);
     });
   });
 
-  describe('selected getter', () => {
+  describe('selected method', () => {
     it('should return the currently selected element in the SelectList', () => {
-      const sel = SelectList(testArr);
+      const sel = new SelectList(testArr);
 
-      expect(sel.selected).toStrictEqual(testArr[0]);
+      expect(sel.selected()).toEqual(testArr[0]);
     });
   });
 
-  describe('before getter', () => {
+  describe('before method', () => {
     it('should return the elements in the `before` section of the SelectList', () => {
-      const sel = SelectList(testArr, 2);
+      const sel = new SelectList(testArr, 2);
 
-      expect(sel.before).toStrictEqual(['a', 'b']);
+      expect(sel.before()).toEqual(['a', 'b']);
+    });
+
+    it("should be a copy of the elements in the `before` section so internal state can't be mutated", () => {
+      const sel = new SelectList(testArr, 2);
+
+      sel.before().push('d');
+
+      expect(sel.before()).toEqual(['a', 'b']);
     });
   });
 
-  describe('after getter', () => {
+  describe('after method', () => {
     it('should return the elements in the `after` section of the SelectList', () => {
-      const sel = SelectList(testArr, 2);
+      const sel = new SelectList(testArr, 2);
 
-      expect(sel.after).toStrictEqual(['d', 'e']);
+      expect(sel.after()).toEqual(['d', 'e']);
     });
   });
 });
