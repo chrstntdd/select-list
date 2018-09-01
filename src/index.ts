d090834ceb7918ea04cc4a53a71683aa3adbdad4
Christian Todd <christiantodd27@icloud.com>
2018-08-31 20:25:48 -0400

    finish class rewrite and prefer in place mutation of map function.

--- src/index.ts
+++ src/index.ts
@@ -1,131 +1,141 @@
-interface SelectList<a> {
+type Position = 'BEFORE' | 'SELECTED' | 'AFTER';
+
+type MaybePieces<a> = [a[], a, a[]] | null;
+
+class SelectListImpl<a> {
+  private _before: a[];
+  private _selected: a;
+  private _after: a[];
+
+  constructor(before: a[], selected: a, after: a[]) {
+    this._before = before;
+    this._selected = selected;
+    this._after = after;
+  }
+
+  private static selectHelp<a>(
+    predicateFn: (element: a) => boolean,
+    before: a[],
+    selected: a,
+    after: a[]
+  ): MaybePieces<a> {
+    // ( [], [] ) ->
+    if (!before.length && !after.length) return null;
+
+    // ( [], head :: rest ) ->
+    if (!before.length && after.length) {
+      const [head, ...rest] = after;
+
+      if (predicateFn(selected)) return [before, selected, after];
+      else if (predicateFn(head)) return [[...before, selected], head, [...after]];
+      else {
+        const mP: MaybePieces<a> = SelectListImpl.selectHelp(predicateFn, [], head, rest);
+
+        if (mP === null) return null;
+        return [[selected, ...mP[0]], mP[1], mP[2]];
+      }
+    }
+
+    // ( head :: rest, _ ) ->
+    const [head, ...rest] = before;
+
+    if (predicateFn(head)) return [[], head, [...rest, selected, ...after]];
+    else {
+      const mP: MaybePieces<a> = SelectListImpl.selectHelp(predicateFn, rest, selected, after);
+
+      if (mP === null) return null;
+      return [[head, ...mP[0]], mP[1], mP[2]];
+    }
+  }
+
   /**
    * @description
-   * Shift to the selected element based on the provided predicate function
+   * Shift the selected element to the first element which passes
+   * the provided predicate function. If no element is found, the
+   * `SelectList` will not be changed.
    */
-  select: <a>(predicateFn: (element: a) => boolean) => SelectList<a>;
+  public select(predicateFn: (element: a) => boolean): SelectListImpl<a> {
+    const mP = SelectListImpl.selectHelp(predicateFn, this._before, this._selected, this._after);
+
+    if (mP === null) return this;
+
+    return new SelectListImpl(mP[0], mP[1], mP[2]);
+  }
+
   /**
    * @description
-   * Apply a function to every element in the SelectList
+   * Apply a function to each element in an array by mutating in place
+   * The API is somewhat in line with the native `Array.map()`, but
+   * instead receives its `Position` type as the third argument
    */
-  map: <b>(fn: (element: a, index?: number, position?: Position) => b) => SelectList<b>;
+  private static mapWithPosition<a, b>(
+    array: a[],
+    callback: (element: a, index?: number, position?: Position) => b,
+    position: Position
+  ): b[] {
+    for (let i = 0; i < array.length; i++) {
+      // @ts-ignore
+      array[i] = callback(array[i], i, position);
+    }
+    // @ts-ignore
+    return array;
+  }
+
   /**
    * @description
-   *s he entire collection as a single array
+   * Apply a transformation function to each element in the `SelectList`.
+   * The transformation function receives a `Position` based on its current
+   * location in the `SelectList`
    */
-  toArray: () => a[];
+  public map<b>(fn: (element: a, index?: number, position?: Position) => b): SelectListImpl<b> {
+    return new SelectListImpl(
+      SelectListImpl.mapWithPosition(this._before, fn, 'BEFORE'),
+      fn(this._selected, this._before.length, 'SELECTED'),
+      SelectListImpl.mapWithPosition(this._after, fn, 'AFTER')
+    );
+  }
+
+  /**
+   * @description
+   * Returns the entire collection as a single array
+   */
+  public toArray(): a[] {
+    return [...this._before, this._selected, ...this._after];
+  }
+
   /**
    * @description
    * Return the size of the entire collection
    */
-  size: () => number;
+  public size(): number {
+    return this._before.length + 1 + this._after.length;
+  }
+
   /**
    * @description
    * The currently selected element
    */
-  selected: a;
+  public get selected() {
+    return this._selected;
+  }
+
   /**
    * @description
-   * The elements currently in the `before` section of the SelectList
+   * The elements currently in the `before` section of the SelectListImpl
    */
-  before: a[];
+  public get before() {
+    return this._before;
+  }
+
   /**
    * @description
-   * The elements currently in the `after` section of the SelectList
+   * The elements currently in the `after` section of the SelectListImpl
    */
-  after: a[];
-}
-
-/**
- * @private
- */
-function selectHelp(
-  predicateFn: (element: any) => boolean,
-  before: any[],
-  selected: any,
-  after: any[]
-): [any[], any, any[]] | null {
-  // ( [], [] ) ->
-  if (!before.length && !after.length) return null;
-
-  // ( [], head :: rest ) ->
-  if (!before.length && after.length) {
-    const [head, ...rest] = after;
-
-    if (predicateFn(selected)) return [before, selected, after];
-    else if (predicateFn(head)) return [[...before, selected], head, [...after]];
-    else {
-      const maybeSelectListParts = selectHelp(predicateFn, [], head, rest);
-
-      if (maybeSelectListParts === null) return null;
-      return [
-        [selected, ...maybeSelectListParts[0]],
-        maybeSelectListParts[1],
-        maybeSelectListParts[2]
-      ];
-    }
-  }
-
-  // ( head :: rest, _ ) ->
-  const [head, ...rest] = before;
-
-  if (predicateFn(head)) return [[], head, [...rest, selected, ...after]];
-  else {
-    const maybeSelectListParts = selectHelp(predicateFn, rest, selected, after);
-
-    if (maybeSelectListParts === null) return null;
-    return [[head, ...maybeSelectListParts[0]], maybeSelectListParts[1], maybeSelectListParts[2]];
+  public get after() {
+    return this._after;
   }
 }
 
-/**
- * @private
- */
-function map(
-  array: any[],
-  callback: (element: any, index?: number, position?: Position) => any,
-  position: Position
-) {
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-    result.push(callback(array[i], i, position));
-  }
-  return result;
+export default function SelectList<a>(before: a[], selected: a, after: a[]) {
+  return new SelectListImpl(before, selected, after);
 }
-
-type Position = 'BEFORE' | 'SELECTED' | 'AFTER';
-
-function SelectList<a>(before: a[], selected: a, after: a[]): SelectList<a> {
-  return {
-    select<a>(predicateFn: (element: a) => boolean): SelectList<a> {
-      const maybeSelectListParts = selectHelp(predicateFn, before, selected, after);
-      if (maybeSelectListParts === null) return this;
-
-      return SelectList(maybeSelectListParts[0], maybeSelectListParts[1], maybeSelectListParts[2]);
-    },
-
-    map<b>(fn: (element: a, index?: number, position?: Position) => b): SelectList<b> {
-      return SelectList(
-        map(before, fn, 'BEFORE'),
-        fn(selected, before.length, 'SELECTED'),
-        map(after, fn, 'AFTER')
-      );
-    },
-
-    toArray(): a[] {
-      return [...before, selected, ...after];
-    },
-
-    size(): number {
-      return before.length + 1 + after.length;
-    },
-
-    selected,
-
-    before,
-
-    after
-  };
-}
-
-export default SelectList;
--- test/SelectList.spec.ts
+++ test/SelectList.spec.ts
@@ -3,74 +3,164 @@ import SelectList from '@/index';
 const testArr = ['a', 'b', 'c', 'd', 'e'];
 
 describe('SelectList', () => {
-  describe('map method', () => {
-    it('should apply a function to each element in the SelectList', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+  describe('transformations', () => {
+    describe('map method', () => {
+      it('should apply a function to each element in the SelectList', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-      const transformation = sel.map((el, i, position) => el.toUpperCase()).toArray();
+        const transformation = sel.map((el, i, position) => el.toUpperCase()).toArray();
 
-      transformation.forEach((el, i) => expect(el).toEqual(sel.toArray()[i].toUpperCase()));
-      expect(sel).not.toEqual(transformation);
+        transformation.forEach((el, i) => expect(el).toEqual(sel.toArray()[i].toUpperCase()));
+        expect(sel).not.toEqual(transformation);
+      });
+
+      it('should get the position as the third argument', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+        let positions = [];
+
+        const transformation = sel
+          .map((el, i, position) => {
+            positions.push(position);
+            return el.toUpperCase();
+          })
+          .toArray();
+
+        transformation.forEach((el, i) => expect(el).toEqual(sel.toArray()[i].toUpperCase()));
+
+        expect(positions[0]).toEqual('BEFORE');
+        expect(positions[1]).toEqual('BEFORE');
+        expect(positions[2]).toEqual('SELECTED');
+        expect(positions[3]).toEqual('AFTER');
+        expect(positions[4]).toEqual('AFTER');
+      });
     });
 
-    it('should get the position as the third argument', () => {
+    describe('select method', () => {
+      it('should no op if the current item is already selected', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+
+        const nextState = sel.select(x => x === 'c');
+
+        expect(nextState.selected).toEqual('c');
+        expect(nextState.before).toEqual(['a', 'b']);
+        expect(nextState.after).toEqual(['d', 'e']);
+      });
+
+      it('should no op if match cant be found', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+
+        const nextState = sel.select(x => x === 'nonexistent');
+
+        expect(nextState.selected).toEqual('c');
+        expect(nextState.before).toEqual(['a', 'b']);
+        expect(nextState.after).toEqual(['d', 'e']);
+      });
+
+      describe('selection cases', () => {
+        const before = [1, 2, 3, 4];
+        const selected = 5;
+        const after = [6, 7, 8, 9];
+        const selectFn = tag => x => x === tag;
+        it('when the selected item is the first item in the `before` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(1));
+
+          expect(nextState.selected).toEqual(1);
+          expect(nextState.before).toEqual([]);
+          expect(nextState.after).toEqual([2, 3, 4, 5, 6, 7, 8, 9]);
+        });
+        it('when the selected item is somewhere in the middle of the `before` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(3));
+
+          expect(nextState.selected).toEqual(3);
+          expect(nextState.before).toEqual([1, 2]);
+          expect(nextState.after).toEqual([4, 5, 6, 7, 8, 9]);
+        });
+        it('when the selected item is the last item in the `before` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(4));
+
+          expect(nextState.selected).toEqual(4);
+          expect(nextState.before).toEqual([1, 2, 3]);
+          expect(nextState.after).toEqual([5, 6, 7, 8, 9]);
+        });
+
+        it.skip('when the selected item is the first item in the `after` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(6));
+
+          expect(nextState.selected).toEqual(6);
+          expect(nextState.before).toEqual([1, 2, 3, 4, 5]);
+          expect(nextState.after).toEqual([7, 8, 9]);
+        });
+
+        it.skip('when the selected item is somewhere in the middle of the `after` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(8));
+
+          expect(nextState.selected).toEqual(8);
+          expect(nextState.before).toEqual([1, 2, 3, 4, 5, 6, 7]);
+          expect(nextState.after).toEqual([9]);
+        });
+
+        it.skip('when the selected item is the last item in the `after` section', () => {
+          const sel = SelectList(before, selected, after);
+          const nextState = sel.select(selectFn(9));
+
+          expect(nextState.selected).toEqual(9);
+          expect(nextState.before).toEqual([1, 2, 3, 4, 5, 6, 7, 9]);
+          expect(nextState.after).toEqual([]);
+        });
+      });
+
       const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
-      let positions = [];
 
-      const transformation = sel
-        .map((el, i, position) => {
-          positions.push(position);
-          return el.toUpperCase();
-        })
-        .toArray();
+      const nextState = sel.select(x => x === 'e');
 
-      transformation.forEach((el, i) => expect(el).toEqual(sel.toArray()[i].toUpperCase()));
-
-      expect(positions[0]).toEqual('BEFORE');
-      expect(positions[1]).toEqual('BEFORE');
-      expect(positions[2]).toEqual('SELECTED');
-      expect(positions[3]).toEqual('AFTER');
-      expect(positions[4]).toEqual('AFTER');
+      expect(nextState.selected).toEqual('e');
+      expect(nextState.before).toEqual(['a', 'b', 'c', 'd']);
+      // expect(nextState.after).toEqual([]);
     });
   });
 
-  describe('toArray method', () => {
-    it('should return the complete collection', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+  describe('reading data', () => {
+    describe('toArray method', () => {
+      it('should return the complete collection', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-      expect(sel.toArray()).toEqual(testArr);
+        expect(sel.toArray()).toEqual(testArr);
+      });
     });
-  });
+    describe('size getter', () => {
+      it('should return length of the complete collection', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-  describe('size method', () => {
-    it('should return length of the complete collection', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
-
-      expect(sel.size()).toEqual(testArr.length);
+        expect(sel.size()).toEqual(testArr.length);
+      });
     });
-  });
+    describe('select getter', () => {
+      it('should return the currently selected element in the SelectList', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-  describe('selected method', () => {
-    it('should return the currently selected element in the SelectList', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
-
-      expect(sel.selected).toEqual('c');
+        expect(sel.selected).toEqual('c');
+      });
     });
-  });
 
-  describe('before method', () => {
-    it('should return the elements in the `before` section of the SelectList', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+    describe('before getter', () => {
+      it('should return the elements in the `before` section of the SelectList', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-      expect(sel.before).toEqual(['a', 'b']);
+        expect(sel.before).toEqual(['a', 'b']);
+      });
     });
-  });
 
-  describe('after method', () => {
-    it('should return the elements in the `after` section of the SelectList', () => {
-      const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
+    describe('after getter', () => {
+      it('should return the elements in the `after` section of the SelectList', () => {
+        const sel = SelectList(['a', 'b'], 'c', ['d', 'e']);
 
-      expect(sel.after).toEqual(['d', 'e']);
+        expect(sel.after).toEqual(['d', 'e']);
+      });
     });
   });
 });
